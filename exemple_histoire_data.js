073ae4cad3fcd811b6f7fc09d080b20898e8de26
LucasVillenave <lucas.villenave33@gmail.com>
2022-12-01 20:56:53 +0100

    Add file

---
+++ exemple_histoire_data.js
@@ -0,0 +1,42 @@
+const data = {
+    1 : {
+        getText() {
+            return "text";
+        },
+        getOptions() {
+            return [{
+                name: "option1",
+                redirect: 2,
+            },
+            {
+                name: "option2",
+                redirect: 3,
+            }]
+        }
+    },
+    2 : {
+        getText() {
+            return "text";
+        },
+        getOptions() {
+            return [{
+                name: "option1",
+                redirect: 2,
+            },
+            {
+                name: "option2",
+                redirect: 3,
+            }]
+        }
+    },
+    3 : {
+        getText() {
+            return "text";
+        },
+        getOptions() {
+            return []
+        }
+    }
+}
+
+// Retourner au choix correct
