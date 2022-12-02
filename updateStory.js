29cb0600a43eacbc982d901ac495ebd93318df8c
emmanueltudela <emmanueltudela.pro@gmail.com>
2022-12-02 05:46:02 +0100

    Commit

--- updateStory.js
+++ updateStory.js
@@ -1,26 +1,14 @@
-let p = document.querySelector("p");
-p.innerHTML = storyData.step1.getText();
+const parentContainer = document.getElementsByClassName("lore")[0];
+parentContainer.innerHTML = get_historicConv();
 
-let inputs = document.getElementsByClassName("inputs");
-let options = storyData.step1.getOptions(); 
+// let p = document.querySelector("p");
+// p.innerHTML = storyData[get_blocName()].getText();
 
+let inputs = document.getElementsByClassName("inputs")[0];
+let options = storyData[get_blockName()].getOptions();
 
-
-function updatePage()
-{
-    inputs[0].innerHTML = "";
-    console.log(inputs.length);
-    const step = storyData[this.value]; 
-    let parent = p.parentNode;
-    
-    let action = document.createElement("p");
-    action.innerHTML = this.innerHTML;
-
-    let newP = document.createElement("p");
-    
-    newP.innerHTML = step.getText();
-    parent.appendChild(action);
-    parent.appendChild(newP);
+function initOptions() {
+    const step = storyData[get_blockName()];
     let options = step.getOptions();
     for(let x in options)
     {
@@ -30,27 +18,72 @@ function updatePage()
         let button = document.createElement("button");
         button.innerHTML = options[x].name;
         button.value = options[x].redirect;
-        button.onclick = updatePage;
+        button.onclick = () => {
+            set_blockName(button.value);
+
+            let action = document.createElement("p");
+            action.innerHTML = button.innerHTML;
+            parentContainer.appendChild(action);
+
+            // Update histroyConv
+            const historicConv = parentContainer.innerHTML;
+            set_historicConv(historicConv)
+
+            updatePage();
+        };
 
         buttonWrapper.appendChild(button);
-        inputs[0].appendChild(buttonWrapper);
+        inputs.appendChild(buttonWrapper);
     }
-
-
+    parentContainer.scrollTo(0, parentContainer.scrollHeight);
 }
 
-
-for(let x in options)
+function updatePage()
 {
-    let buttonWrapper = document.createElement("div");
-    buttonWrapper.classList.add("buttonWrapper");
+    const step = storyData[get_blockName()];
+    inputs.innerHTML = "";
 
-    let button = document.createElement("button");
-    button.innerHTML = options[x].name;
-    button.value = options[x].redirect;
-    button.onclick = updatePage;
+    let newP = document.createElement("p");
+    
+    newP.innerHTML = step.getText();
+    parentContainer.appendChild(newP);
 
-    buttonWrapper.appendChild(button);
-    inputs[0].appendChild(buttonWrapper);
+    initOptions();
+
+    // Update histroyConv
+    const historicConv = parentContainer.innerHTML;
+    set_historicConv(historicConv)
+
+    parentContainer.scrollTo(0, parentContainer.scrollHeight);
+
+    if (restartGame) {
+        localStorage.clear()
+
+        let buttonWrapper = document.createElement("div");
+        buttonWrapper.classList.add("buttonWrapper");
+
+        let button = document.createElement("button");
+        button.innerHTML = "Restart";
+        button.onclick = () => {
+            window.location.reload()
+        };
+        buttonWrapper.appendChild(button);
+        inputs.appendChild(buttonWrapper);
+    }
 }
 
+initOptions();
+
+// for(let x in options)
+// {
+//     let buttonWrapper = document.createElement("div");
+//     buttonWrapper.classList.add("buttonWrapper");
+
+//     let button = document.createElement("button");
+//     button.innerHTML = options[x].name;
+//     button.value = options[x].redirect;
+//     button.onclick = updatePage;
+
+//     buttonWrapper.appendChild(button);
+//     inputs[0].appendChild(buttonWrapper);
+// }
